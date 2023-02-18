93d6c1bc5687f4d6f14c6c10443d8a0fb6056360
anastasia <anastasia.lurye@lean-ai-tech.com>
2023-02-18 23:16:43 +0200

    augmentation not working

--- main2.py
+++ main2.py
@@ -18,17 +18,29 @@ tf.random.set_seed(1)
 batch_size = 32
 epochs = 40
 learning_rate = 0.001
-n_workers = 8
+n_workers = 1
+
+# Use ImageDataGenerator to apply data augmentation
+train_datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
 
 # Download data
 cars_test, cars_val, cars_train = tfds.load('Cars196', data_dir='C:/Users/anast/PycharmProjects/cars196/data',
                                             as_supervised=False,
                                             shuffle_files=True, split=["test", "train[0%:20%]", "train[20%:]"])
 
-
+from keras.utils import load_img, img_to_array
 # Iterate over the dataset and crop the images using the bounding box information
 def preprocess_image(example):
     image = example['image']
+    print('before: ', image.shape)
     bbox = example['bbox']
     height, width = tf.unstack(tf.shape(image)[:2])
     scaled_box = bbox * [height, width, height, width]
@@ -41,11 +53,27 @@ def preprocess_image(example):
     image = tf.image.resize_with_pad(image, 224, 224)
     image = preprocess_input(image)
     label = tf.one_hot(example['label'], 196)
+    print('after: ', image.shape)
 
     return image, label
+def tfds_imgen(ds, imgen, batch_size, batches_per):
+    for images, labels in ds:
+        flow_ = imgen.flow(images, labels, batch_size=batch_size)
+        for _ in range(batches_per):
+            yield next(flow_)
+# for example in cars_train:
+#     image, label = example['image'], example['label']
+#     train_datagen.fit(np.array([image]))
+# # datagen.flow(x_train,y_train, batch_size=6)
+# train_datagen.fit(cars_train)
+
 
 cars_train_pp = cars_train.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(batch_size)
 cars_val_pp = cars_val.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE).batch(batch_size)
+
+cars_train_pp = tfds_imgen(
+    cars_train_pp.as_numpy_iterator(), train_datagen,
+    batch_size=32, batches_per= 32)
 
 # Dictionary of the labels - maps between the label (int) number and the  vehicle model (str)
 label_dic = pd.read_csv('/home/anastasia/Downloads/labels_dic.csv', header=None, dtype={0: str}).\
@@ -70,22 +98,11 @@ output = prediction_layer
 model = Model(inputs=base_model.input, outputs=output)
 model.summary()
 
-# Use ImageDataGenerator to apply data augmentation
-train_datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    preprocessing_function=preprocess_input
-)
 
 # Train model
 opt = Adam(lr=learning_rate)
 loss_fn = keras.losses.CategoricalCrossentropy(from_logits=False)
 model.compile(optimizer=opt, loss=loss_fn, metrics=['accuracy'])
-cars_train_pp = train_datagen.flow(cars_train_pp, batch_size=batch_size) # <-------- the part that doesn't work
 hist = model.fit(cars_train_pp, epochs=epochs, validation_data=cars_val_pp, workers=n_workers)
 model.save('model')
 
